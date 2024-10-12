9e54bc38c6d02e848a783ce94a2ae06687756f05
RikuRoivainen <bhd722@myy.haaga-helia.fi>
2024-10-12 13:39:34 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,20 +1,134 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import { TextInput, Button, Card, List, Provider as PaperProvider, IconButton, Appbar } from 'react-native-paper';
+import * as SQLite from 'expo-sqlite';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [product, setProduct] = useState('');
+  const [amount, setAmount] = useState('');
+  const [items, setItems] = useState([]);
+
+  const db = SQLite.openDatabaseSync('itemsdb');
+
+  const initialize = async () => {
+    try {
+      await db.execAsync(`
+        CREATE TABLE IF NOT EXISTS item (id INTEGER PRIMARY KEY AUTOINCREMENT, product TEXT, amount TEXT);
+        `);
+      await updateList();
+    } catch (error) {
+      console.error('Could not open database', error);
+    }
+  }
+
+  useEffect(() => { initialize() }, []);
+
+  const addItem = async () => {
+    try {
+      await db.runAsync('INSERT INTO item VALUES (?, ?, ?)', null, product, amount);
+      await updateList();
+      console.log(items);
+      setProduct(''); 
+      setAmount('');
+    } catch (error) {
+      console.error('Could not add item', error);
+    }
+  };
+
+  const updateList = async () => {
+    try {
+      const list = await db.getAllAsync('SELECT * from item');
+      setItems(list);
+    } catch (error) {
+      console.error('Could not get items', error);
+    }
+  };
+
+  const deleteItem = async (id) => {
+    try {
+      await db.runAsync('DELETE FROM item WHERE id=?', id);
+      await updateList();
+
+    } catch (error) {
+      console.error('Could not delete item', error);
+    }
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Appbar.Header>
+          <Appbar.Content title="Shopping list" />
+        </Appbar.Header>
+        <Card style={styles.card}>
+          <Card.Content>
+            <TextInput
+              mode="outlined"
+              label="Product"
+              value={product}
+              onChangeText={setProduct}
+              style={styles.input}
+            />
+            <TextInput
+              mode="outlined"
+              label="Amount"
+              value={amount}
+              onChangeText={setAmount}
+              style={styles.input}
+            />
+            <Button mode="contained" 
+              onPress={addItem} 
+              style={styles.button}
+              icon="content-save">
+              Save
+            </Button>
+          </Card.Content>
+        </Card>
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) =>
+            <Card style={styles.itemCard}>
+              <Card.Content>
+                <List.Item
+                  title={item.product}
+                  description={`Amount: ${item.amount}`}
+                  right={props => (
+                    <IconButton
+                      icon="delete"
+                      iconColor="red"
+                      onPress={() => deleteItem(item.id)}
+                      />
+                  )}
+                />
+              </Card.Content>
+            </Card>}
+          data={items}
+        />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f8f9fa',
+  },
+  card: {
+    marginBottom: 20,
+    marginHorizontal: 20, 
+  },
+  input: {
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 10,
+  },
+  itemCard: {
+    marginBottom: 10,
+    marginHorizontal: 20, 
   },
 });
